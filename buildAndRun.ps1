e6702c105a19d00046f18d180f713f7b368e1a5a
djfdyuruiry <djfdyuruiry@users.noreply.github.com>
2018-01-03 22:02:07 +0000

    Adding process killing to dev script

--- buildAndRun.ps1
+++ buildAndRun.ps1
@@ -1,2 +1,41 @@
+function GetProcessesUsingPortsOnWindows
+{
+    $processes = @((netstat -o -n -a | findstr ":8888"), `
+        (netstat -o -n -a | findstr ":4430"))   
+
+    $pids = @()
+
+    if ($processes.Length -gt 0)
+    {
+        foreach ($process in $processes)
+        {
+            $columns = $process -split "\s+"
+            $pids += $columns[-1]
+        }
+    }
+
+    ,$pids
+}
+
+
 & "$PSScriptRoot/build.ps1"
+
+$pidsUsingPorts
+
+if ($PSVersionTable.Platform -eq "Unix")
+{
+    $pidsUsingPorts += lsof -ti tcp:8888
+    $pidsUsingPorts += lsof -ti tcp:4430
+}
+else
+{
+    $pidsUsingPorts = GetProcessesUsingPortsOnWindows
+}
+
+foreach ($pidUsingPort in $pidsUsingPorts)
+{
+    Write-Host "Killing process with id $pidUsingPort currently using port 8888/4430"
+    Stop-Process -Id $pidUsingPort -Force
+}
+
 & "$PSScriptRoot/build/release/startServer.ps1"
